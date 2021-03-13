d5d2fb4ce2bc4297ac6d530471fa51710d346102
sbkim23 <za2322@naver.com>
2021-03-13 20:03:48 +0900

    jpa test

--- src/main/java/hello/hellospring/domain/Member.java
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -1,6 +1,12 @@
 package hello.hellospring.domain;
 
+import javax.persistence.*;
+
+@Entity
 public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 
     private Long id;
     private String name;
